0285f7b08ddcf007403efc6c63a89880492bb4f2
Jim Garrison <jim@garrison.cc>
2015-01-26 22:15:37 -0800

    much implementation

--- src/IndexedArrays.jl
+++ src/IndexedArrays.jl
@@ -1,5 +1,67 @@
 module IndexedArrays
 
-# package code goes here
+include("delegate.jl")
+
+import Base: copy, in, getindex, findfirst, length, size, start, done, next, empty!, push!
+
+abstract AbstractIndexedArray{T} <: AbstractVector{T}
+
+type IndexedArrayError <: Exception # FIXME: or should we just use ArgumentError here?
+    msg::AbstractString
+end
+
+immutable IndexedArray{T} <: AbstractIndexedArray
+    items::Array{T}
+    lookup::Dict{T,Int}
+
+    IndexedArray() = new(T[], Dict{T,Int}())
+    function IndexedArray(items::Array{T})
+        ia = new(items, Dict{T,Int}())
+        sizehint!(ia.lookup, length(ia.items))
+        for (i, item) in enumerate(ia.items)
+            if item in ia
+                throw(IndexedArrayError())
+            end
+            ia.lookup[item] = i
+        end
+        return ia
+    end
+end
+
+@delegate IndexedArray.items [ length, size, start, done, next ]
+
+getindex(ia::IndexedArray, i::Int) = getindex(ia.items, i)
+getindex(ia::IndexedArray, r::UnitRange{Int}) = getindex(ia.items, r)
+
+function empty!(ia::IndexedArray)
+    # NOTE: does not provide any exception safety guarantee
+    empty!(ia.items)
+    empty!(ia.lookup)
+    return ia
+end
+
+in(item, ia::IndexedArray) = haskey(ia.lookup, item)
+
+findfirst(ia::IndexedArray, item) = ia.lookup[item] # throws KeyError if not found
+
+findfirst!(ia::IndexedArray, item) = get!(ia.lookup, item) do
+    # NOTE: does not provide any exception safety guarantee
+    push!(ia.items, item)
+    ia.lookup[item] = length(ia)
+end
+
+function push!(ia::IndexedArray, item)
+    if item in ia
+        throw(IndexedArrayError())
+    end
+    # NOTE: does not provide any exception safety guarantee
+    push!(ia.items, item)
+    ia.lookup[item] = length(ia)
+    return ia
+end
+
+copy{T}(ia::IndexedArray{T}) = IndexedArray{T}(copy(ia.items))
+
+export AbstractIndexedArray, IndexedArray, findfirst!
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,53 @@
 using IndexedArrays
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+ia = IndexedArray{ASCIIString}()
+
+@test isempty(ia)
+@test_throws KeyError findfirst(ia, "cat")
+@test findfirst!(ia, "cat") == 1
+@test !isempty(ia)
+@test "cat" in ia
+@test "dog" ∉ ia
+@test findfirst!(ia, "dog") == 2
+@test findfirst!(ia, "cat") == 1
+@test findfirst!(ia, "mouse") == 3
+@test findfirst!(ia, "dog") == 2
+@test findfirst(ia, "cat") == 1
+@test findfirst(ia, "dog") == 2
+@test findfirst(ia, "mouse") == 3
+@test ia[1] == "cat"
+@test ia[2] == "dog"
+@test ia[3] == "mouse"
+@test ia[:] == ["cat", "dog", "mouse"]
+@test size(ia) == (3,)
+@test length(ia) == 3
+@test endof(ia) == 3
+
+elts = Set(["cat", "dog", "mouse"])
+for elt in ia
+    pop!(elts, elt)
+end
+@test isempty(elts)
+
+ia2 = copy(ia)
+
+empty!(ia)
+@test isempty(ia)
+@test_throws KeyError findfirst(ia, "cat")
+@test findfirst!(ia, "horse") == 1
+
+@test ia2[:] == ["cat", "dog", "mouse"]
+@test findfirst(ia2, "cat") == 1
+
+# push!, pop!
+
+# setindex!, delete!
+
+# FIXME: test trying to add something that already exists
+
+# FIXME: test sending invalid stuff to the constructors
+
+# indexin, findin
+
+# findlast?, findnext, findprev
