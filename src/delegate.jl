f4b28bfbb34b5df94f919a468dec34b27342b513
Jim Garrison <jim@garrison.cc>
2017-10-13 17:56:19 -0700

    Fix delegate.jl for julia master

--- src/delegate.jl
+++ src/delegate.jl
@@ -1,15 +1,27 @@
 # by JMW; taken from DataStructures.jl
+
+# See https://github.com/JuliaCollections/DataStructures.jl/commit/bb7b51f95a9ddc7582acb171486f150353c6e361
+function unquote(e::Expr)
+    # For julia < 0.7
+    @assert e.head == :quote
+    return e.args[1]
+end
+
+function unquote(e::QuoteNode)
+    return e.value
+end
+
 macro delegate(source, targets)
     typename = esc(source.args[1])
-    fieldname = esc(Expr(:quote, source.args[2].args[1]))
+    fieldname = unquote(source.args[2])
     funcnames = targets.args
     n = length(funcnames)
-    fdefs = Array{Any}(n)
+    fdefs = Vector{Any}(n)
     for i in 1:n
         funcname = esc(funcnames[i])
         fdefs[i] = quote
                      ($funcname)(a::($typename), args...) =
-                       ($funcname)(getfield(a, $fieldname), args...)
+                       ($funcname)(a.$fieldname, args...)
                    end
     end
     return Expr(:block, fdefs...)
